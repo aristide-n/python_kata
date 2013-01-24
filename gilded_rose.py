2d0e9cda4065660dd926863d0f72bdd304334221
Aristide Niyungeko <niyungeko@gmail.com>
2013-01-23 20:12:45 -0800

    solved gilded rose

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,35 +1,45 @@
 # -*- coding: utf-8 -*-
 
+def update_aged(item):
+    item.sell_in -= 1
+    if item.quality < 50: item.quality += 1
+
+
+def update_backstage(item):
+    item.sell_in -= 1
+
+    if item.sell_in < 0: item.quality = 0
+    elif item.quality < 50:
+        item.quality += 1
+        if item.quality < 50 and item.sell_in < 10: item.quality += 1
+        if item.quality < 50 and item.sell_in < 5: item.quality += 1
+
+def update_conjured(item):
+    item.sell_in -= 1
+    if item.quality > 0: item.quality -= 2
+
+    if item.sell_in < 0 and item.quality > 0: item.quality -= 2
+
+
+def update_standard(item):
+    item.sell_in -= 1
+    if item.quality > 0: item.quality -= 1
+
+    if item.sell_in < 0 and item.quality > 0: item.quality -= 1
+
 
 def update_quality(items):
     for item in items:
-        if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-            if item.quality > 0:
-                if item.name != "Sulfuras, Hand of Ragnaros":
-                    item.quality = item.quality - 1
-        else:
-            if item.quality < 50:
-                item.quality = item.quality + 1
-                if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    if item.sell_in < 11:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-                    if item.sell_in < 6:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-        if item.name != "Sulfuras, Hand of Ragnaros":
-            item.sell_in = item.sell_in - 1
-        if item.sell_in < 0:
-            if item.name != "Aged Brie":
-                if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                    if item.quality > 0:
-                        if item.name != "Sulfuras, Hand of Ragnaros":
-                            item.quality = item.quality - 1
-                else:
-                    item.quality = item.quality - item.quality
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
+        if item.name == "Aged Brie":
+            update_aged(item)
+        elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+            update_backstage(item)
+        elif item.name == "Conjured bow tie":
+            update_conjured(item)
+        elif item.name != "Sulfuras, Hand of Ragnaros":
+            update_standard(item)
+        # Last: Do nothing for sulfuras items
+
     return items
 
 
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -4,11 +4,91 @@ from gilded_rose import Item, update_quality
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        update_quality(items)
-        self.assertEquals("fixme", items[0].name)
 
+    def test_standard_update(self):
+        items = [Item("foo", 10, 40)]
+
+        # Update lowers quality and sell In days by 1
+        update_quality(items)
+        self.assertEquals(9, items[0].sell_in)
+        self.assertEquals(39, items[0].quality)
+
+        # Update past the sell date lowers the quality by 2
+        for _ in range(10): update_quality(items)
+        self.assertEqual(28, items[0].quality)
+
+        # The quality is never negative
+        for _ in range(20): update_quality(items)
+        self.assertEqual(0, items[0].quality)
+
+
+    def test_aged_update(self):
+        items = [Item("Aged Brie", 10, 40)]
+
+        # Update lowers sell In days by 1 and increases quality by 1
+        update_quality(items)
+        self.assertEquals(9, items[0].sell_in)
+        self.assertEquals(41, items[0].quality)
+
+        # The quality is never greater than 50
+        for _ in range(60): update_quality(items)
+        self.assertEqual(50, items[0].quality)
+
+
+    def test_sulfuras_update(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 10, 40)]
+
+        # Update does nothing to this item
+        update_quality(items)
+        self.assertEquals(10, items[0].sell_in)
+        self.assertEquals(40, items[0].quality)
+
+
+    def test_backstage_update(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 20, 20)]
+
+        # Update with more than 10 sell in days increases quality by 1 and lowers
+        # sell In days by 1
+        update_quality(items)
+        self.assertEquals(19, items[0].sell_in)
+        self.assertEquals(21, items[0].quality)
+
+        # Update with 10 - 6 sell in days increases quality by 2 and lowers
+        # sell In days by 1
+        for _ in range(10): update_quality(items)
+        self.assertEquals(9, items[0].sell_in)
+        self.assertEquals(32, items[0].quality)
+
+        # Update with 5 - 1 sell in days increases quality by 3 and lowers
+        # sell In days by 1
+        for _ in range(5): update_quality(items)
+        self.assertEquals(4, items[0].sell_in)
+        self.assertEquals(43, items[0].quality)
+
+        # The quality is never greater than 50
+        for _ in range(4): update_quality(items)
+        self.assertEqual(50, items[0].quality)
+
+        # Update past the concert date date lowers the quality to 0
+        update_quality(items)
+        self.assertEqual(0, items[0].quality)
+
+
+    def test_conjured_update(self):
+        items = [Item("Conjured bow tie", 10, 40)]
+
+        # Update lowers quality by 2 and sell In days by 1
+        update_quality(items)
+        self.assertEquals(9, items[0].sell_in)
+        self.assertEquals(38, items[0].quality)
+
+        # Update past the sell date lowers the quality by 4
+        for _ in range(10): update_quality(items)
+        self.assertEqual(16, items[0].quality)
+
+        # The quality is never negative
+        for _ in range(20): update_quality(items)
+        self.assertEqual(0, items[0].quality)
 
 if __name__ == "__main__":
     unittest.main()
