3d6e1006f0a6a6bf15c342fcb171161690d92942
Aristide Niyungeko <niyungeko@gmail.com>
2013-02-12 02:26:12 -0800

    potter kata: simple greedy algorithm, not optimal

--- potter.py
+++ potter.py
@@ -1,1 +1,47 @@
 __author__ = 'Aristide'
+
+
+def count_same_book(items):
+    counts = [0,0,0,0,0]
+
+    counts[0] = items.count(0)
+    counts[1] = items.count(1)
+    counts[2] = items.count(2)
+    counts[3] = items.count(3)
+    counts[4] = items.count(4)
+
+    return counts
+
+def price(items):
+    """
+    Algorithm description (simple greedy algorithm):
+        1. Sort the list of books
+        2. Create an ordered list of 5 items, each item is the count of each book in the basket, from book 1 to book 5
+        5. Iterate through the list until the sum of the items is 0 (empty basket). Per iteration:
+            a. add 1 to a discount ID for each book of a different type
+            b. remove 1 for the count of that book
+            c. the discount ID is both the count of items, and the index of the discount. At the end of the iteration,
+               multiply the two and the item price (8) and add that to the total price
+
+    """
+
+    total = 0
+
+    sorted(items)
+
+    book_counts = count_same_book(items)
+
+    discounts = [None, 1, .95, .9, .8, .75]
+
+    discount_id = 0
+
+    while sum(book_counts) > 0:
+        for index, value in enumerate(book_counts):
+            if value > 0:
+                discount_id += 1
+                book_counts[index] -= 1
+
+        total += discount_id * 8 * discounts[discount_id]
+        discount_id = 0
+
+    return total
--- test_potter.py
+++ test_potter.py
@@ -1,1 +1,44 @@
 __author__ = 'Aristide'
+
+from nose.tools import assert_equal, assert_true
+from unittest import TestCase
+from potter import price
+
+class TestPotterPrice(TestCase):
+
+    def testBasics(self):
+        assert_equal(0, price([]))
+        assert_equal(8, price([0]))
+        assert_equal(8, price([1]))
+        assert_equal(8, price([2]))
+        assert_equal(8, price([3]))
+        assert_equal(8, price([4]))
+        assert_equal(8 * 2, price([0, 0]))
+        assert_equal(8 * 3, price([1, 1, 1]))
+
+
+    def testSimpleDiscounts(self):
+        assert_equal(8 * 2 * 0.95, price([0, 1]))
+        assert_equal(8 * 3 * 0.9, price([0, 2, 4]))
+        assert_equal(8 * 4 * 0.8, price([0, 1, 2, 4]))
+        assert_equal(8 * 5 * 0.75, price([0, 1, 2, 3, 4]))
+
+
+    def testSeveralDiscounts(self):
+        assert_equal(8 + (8 * 2 * 0.95), price([0, 0, 1]))
+        assert_equal(2 * (8 * 2 * 0.95), price([0, 0, 1, 1]))
+        assert_equal((8 * 4 * 0.8) + (8 * 2 * 0.95), price([0, 0, 1, 2, 2, 3]))
+        assert_equal(8 + (8 * 5 * 0.75), price([0, 1, 1, 2, 3, 4]))
+
+
+    def testEdgeCases(self):
+        assert_equal(2 * (8 * 4 * 0.8), price([0, 0,
+                                               1, 1,
+                                               2, 2,
+                                               3, 4]))
+        assert_equal(3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8),
+                     price([0, 0, 0, 0, 0,
+                            1, 1, 1, 1, 1,
+                            2, 2, 2, 2, 3,
+                            3, 3, 3, 3, 4,
+                            4, 4, 4]))
