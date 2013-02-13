c7d1d78781991383cb9073e6be7cad8e362d455f
Aristide Niyungeko <niyungeko@gmail.com>
2013-02-12 16:37:17 -0800

    Potter: Final implementation

--- potter.py
+++ potter.py
@@ -1,5 +1,8 @@
 __author__ = 'Aristide'
 
+"""
+Potter Kata: http://craftsmanship.sv.cmu.edu/katas/potter-kata
+"""
 
 def count_same_book(items):
     counts = [0,0,0,0,0]
@@ -14,15 +17,34 @@ def count_same_book(items):
 
 def price(items):
     """
-    Algorithm description (simple greedy algorithm):
+    The trick of the potter Kata is that there is a gap of 10% for a discount for 3 books and a discount for 4 books.
+    The resulting behavior is that the discount for a set of 5 books and a set of 3 books is less than the discount
+    for 2 sets of 4 books. A simple greedy algorithm does not satisfy the desired scheme.
+
+    This is my optimal Algorithm for Potter:
+    (case's solution; inspired by http://ardalis.com/a-first-pass-at-potterkata)
         1. Sort the list of books
         2. Create an ordered list of 5 items, each item is the count of each book in the basket, from book 1 to book 5
-        5. Iterate through the list until the sum of the items is 0 (empty basket). Per iteration:
-            a. add 1 to a discount ID for each book of a different type
-            b. remove 1 for the count of that book
-            c. the discount ID is both the count of items, and the index of the discount. At the end of the iteration,
-               multiply the two and the item price (8) and add that to the total price
+        3. Iterate through the list until the sum of the items is 0 (empty basket). Per iteration:
+            I. For each book of a different type:
+                a. check if there is 2 or more of that type or that we have already found exactly 3 different books
+                    with the previous property
+                b. add 1 to a discount ID (the discount ID is both the count of different items, and the indicator of
+                    the discount.)
+                c. remove 1 from the count of that type
 
+                What the above does is that we exhaust discounts on all the possible sets of 4 before any set of 3.
+                Case in point: For this set: [0, 0, the algorithm is smart to increment discount ID for item 3, but
+                                              1, 1,   when it get to 4 the discount ID == 3 fails and the discount
+                                              2, 2,   for 4 books is applied. The next iteration hits II.
+                                              3,
+                                              4]
+
+            II. If discount ID is 0 it means that no book type accounts for more than 1 item. do b and c from I
+
+            At the end of the iteration, multiply the count of different items (discount ID) with the discount value
+            and the item price (8), and add that to the total price. If II ran in the iteration, it will be the
+            end of the loop because the count of the units of each book type will be 0.
     """
 
     total = 0
@@ -35,26 +57,12 @@ def price(items):
 
     discount_id = 0
 
-    # filter 0 counts
-
-
     while sum(book_counts) > 0:
 
-        remaining_books = filter(lambda x: x > 0, book_counts)
-
-        print 'start'
-        print remaining_books
-        print book_counts
-
         for index, value in enumerate(book_counts):
-            if len(book_counts) > 3:
-                if value > 1 or discount_id == 3:
-                    discount_id += 1
-                    book_counts[index] -= 1
-
-        print 'cont'
-        print remaining_books
-        print book_counts
+            if value > 1 or discount_id == 3:
+                discount_id += 1
+                book_counts[index] -= 1
 
         if discount_id == 0:
              for index, value in enumerate(book_counts):
@@ -62,13 +70,7 @@ def price(items):
                     discount_id += 1
                     book_counts[index] -= 1
 
-        print discount_id
-
         total += discount_id * 8 * discounts[discount_id]
         discount_id = 0
-
-        print 'end'
-        print remaining_books
-        print book_counts
 
     return total
