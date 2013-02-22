9fff86933a0a13e7ba84ac9146c340f809bd2ff1
Aristide Niyungeko <niyungeko@gmail.com>
2013-02-21 17:47:06 -0800

    coin changer Kata: initialize tests

--- coin_changer.py
+++ coin_changer.py
@@ -1,1 +1,20 @@
 __author__ = 'Aristide'
+
+from nose.tools import assert_equal
+from unittest import TestCase
+
+
+
+
+class TestCoinChanger(TestCase):
+
+    def placeholder(self):
+        NotImplemented
+
+
+
+
+
+
+def coin_changer(amount):
+    NotImplemented
